c0a567c00e02cc62c88f584312bc6851319a01ea
AishwaryaDayanand <aishaishwarya914@gmail.com>
2022-09-19 17:21:36 +0530

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'learning';
+  name : string;   //declaration
+  age : number;
+  title : string | number;
+
+  constructor(){
+    this.name = "Aishwarya" // initialization
+    this.age = 21;
+    this.title = "2qq";
+    // this.handlerMethod('string',2)
+  }
+
+  handlerMethod1(var1 : string , var2 : number) :
+   void {
+    console.log(`handler method called:  ${var1} ${var2}`);
+
+  }
+
+  handlerMethod2(var1 : string , var2 : string) : 
+  string[] {
+    let arr : string[] = [var1,var2];
+    console.log(`handler method called:  ${var1} ${var2}`);
+    console.log(arr);
+    return arr ; 
+
+  }
+
+  handlerMethod3(var1 : number , var2 : number) : {var1:number , var2 :number}{
+    console.log(`handler method called:  ${var1} ${var2}`);
+    let obj = {var1:var1 , var2 :var2}
+    console.log(obj);
+    return obj;
+
+  }
 }
+
